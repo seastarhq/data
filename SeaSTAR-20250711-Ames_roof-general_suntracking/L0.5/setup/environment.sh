77e6624ee3696e2084deedda3eafffbde0e0ecf5
Stephen Broccardo <sbroccardo@gmail.com>
2026-05-06 10:34:46 -0700

    Updated error_flags.py in 0710

--- SeaSTAR-20250711-Ames_roof-general_suntracking/L0.5/setup/environment.sh
+++ SeaSTAR-20250711-Ames_roof-general_suntracking/L0.5/setup/environment.sh
@@ -6,3 +6,8 @@ PLOTS_DIR=$ROOT_DIR/L0.5/plots
 PICKLE_DIR=$ROOT_DIR/pickle
 
 export ROOT_DIR SCRIPTS_DIR RAW_DATA_DIR EXTRACTED_DATA_DIR PLOTS_DIR PICKLE_DIR
+
+
+#  you should modify this for your own environment, then do
+# git update-index --skip-worktree <this_file>
+# so that it doesn't get updated on the github
