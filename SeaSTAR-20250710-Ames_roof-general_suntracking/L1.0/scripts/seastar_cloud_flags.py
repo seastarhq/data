640dc9343f08577c7444dbce635a559cfc282f2e
Stephen Broccardo <sbroccardo@gmail.com>
2026-05-19 14:06:08 -0700

    Added L1 code

---
+++ SeaSTAR-20250710-Ames_roof-general_suntracking/L1.0/scripts/seastar_cloud_flags.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# flag locations in the L05 array:
+# L05line['flags'][0] is tracking flags
+# L05line['flags'][1] is robot flags
+# L05line['flags'][2] is housekeeping flags
+#                   [3] is 1x radiometer flags
+#                   [4] is 100x radiometer flags
+#                   [5] is 10kx radiometer flags
